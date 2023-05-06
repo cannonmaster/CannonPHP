64d822bc173458c0f4d5deb1405e5b1734c3a175
cannon <cannonmaster666@gmail.com>
2023-05-06 04:45:22 -0700

    add empty folder of cache and log

---
+++ Cache/2d/2d2063ce6771a830df76115a008d0c61.php
@@ -0,0 +1,76 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* Home/index.html */
+class __TwigTemplate_0de725b866755761c6162c2eb9487933 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+
+<head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>";
+        // line 12
+        echo twig_escape_filter($this->env, ($context["name"] ?? null), "html", null, true);
+        echo "</h1>
+</body>
+
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "Home/index.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 12,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "Home/index.html", "/Users/h2o/Sites/localhost/framework/App/View/Home/index.html");
+    }
+}
